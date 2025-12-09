9868151b5e9334ab8daae9f24be503ffc1b3c672
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-12-09 04:46:56 +0100

    style: Added styles and background dots

--- app/ui/extension-card.tsx
+++ app/ui/extension-card.tsx
@@ -9,11 +9,12 @@ type CardProps = {
 
 export default function Card(props: CardProps) {
     return(
-        <div>
-            <Image src={props.image} width={200} height={200} alt="" />
-            <p>{props.title}</p>
+        <div className="grid place-items-center gap-3 py-12 rounded-lg card-shadow">
+            <Image src={props.image} width={100} height={100} alt="" />
+            <p className='text-xl font-medium text-center'>{props.title}</p>
             <p>{props.version}</p>
-            <button>{props.install}</button>
+            <Image className="h-1 w-full" src="/bg-dots.svg" alt="" width={100} height={100} />
+            <button className="my-4 py-4 px-6 rounded-lg bg-blue-600 text-white">{props.install}</button>
         </div>
     )
 }
