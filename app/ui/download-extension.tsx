cfb9ef99dcbbfb0616dfda361ca46693dccdd551
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-12-09 04:47:28 +0100

    style: Added styles

--- app/ui/download-extension.tsx
+++ app/ui/download-extension.tsx
@@ -3,22 +3,24 @@ import Card from './extension-card';
 
 export default function ExtensionDownload () {
     return (
-        <section>
-            <h2>Download the extension</h2>
-            <p>
+        <section className='grid gap-4'>
+            <h2 className='text-2xl font-bold text-center'>Download the extension</h2>
+            <p className='text-center text-width'>
                 We've got more browsers in the pipeline. Please
                 do let us know if you've got a favourite  you'd like us
                 to prioritize
             </p>
-            {data.map((info) => (
-                <Card
-                    key={info.id}
-                    image={info.image}
-                    title={info.title}
-                    version={info.version}
-                    install={info.install}
-                />
-            ))}
+            <div className='*:mx-6 *:my-12'>
+                {data.map((info) => (
+                    <Card
+                        key={info.id}
+                        image={info.image}
+                        title={info.title}
+                        version={info.version}
+                        install={info.install}
+                    />
+                ))}
+            </div>
         </section>
     )
 }
