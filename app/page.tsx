183dd0517594f129f7b8f8ff33030d3f9d6426a9
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-12-09 04:42:03 +0100

    feat: decorative backdrop added, style: button styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,13 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
-      <main>
+      <main className="flex flex-col gap-12 items-center justify-center">
         <Navigation />
         <section className="flex items-center justify-center flex-col md:flex-row gap-4">
-          <Image src="/illustration-hero.svg" alt="" width={350} height={350} />
-          {/* <div>blue background stuff</div> */}
+          <div className="relative">
+            <Image src="/illustration-hero.svg" alt="" width={350} height={350} />
+            <div className="bg-blue-500 w-11/12 h-3/4 rounded-l-full blue-backdrop"></div>
+          </div>
           <div>
             <h1 className="text-4xl font-bold text-center">A Simple bookmark manager</h1>
             <p className="text-width text-center md:text-left">
@@ -21,8 +23,8 @@ export default function Home() {
               sites load instantly. Try it for free.
             </p>
             <div className="flex items-center justify-center gap-4 my-4">
-              <button className="py-5 px-4 rounded-lg bg-black">Get it in Chrome</button>
-              <button className="py-5 px-4 rounded-lg bg-black">Get it in Firefox</button>
+              <button className="py-5 px-4 rounded-lg bg-blue-600 text-white">Get it in Chrome</button>
+              <button className="py-5 px-4 rounded-lg bg-grey-200 text-black drop-shadow-md">Get it in Firefox</button>
             </div>
           </div>
         </section>
