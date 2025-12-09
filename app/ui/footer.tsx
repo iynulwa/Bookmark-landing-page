63a8602b86eed0ed96f785196b93bb9a62e2146f
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-12-09 04:40:21 +0100

    style: colour and alignment additions

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,25 +1,25 @@
 import Image from 'next/image'
+import Search from './search'
 
 export default function Footer() {
     return(
-        <section>
-            <div>
-                <p>35,000+ ALREADY JOINED</p>
-                <p>Stay up-to-date with what we're doing</p>
-                {/* search box componenet */}
+        <section className='w-full'>
+            <div className='bg-blue-400 grid place-items-center py-20'>
+                <p className='text-white font-normal tracking-widest'>35,000+ ALREADY JOINED</p>
+                <p className='text-2xl text-center text-white font-medium'>Stay up-to-date with what we're doing</p>
+                <Search />
             </div>
-            <div>
-                <Image width={150} height={150} src="/logo-bookmark.svg" alt='' />
-                <ul>
+            <div className='grid place-items-center gap-6 bg-blue-900 py-10'>
+                <Image className="filter brightness-0 invert" width={150} height={150} src="/logo-bookmark.svg" alt='' />
+                <ul className='text-white text-center *:mb-4'>
                     <li>FEATURES</li>
                     <li>PRICING</li>
                     <li>CONTACT</li>
                 </ul>
-                <div className='flex'>
-                    <Image src="/icon-facebook.svg" alt='Go to facebook' width={50} height={50} />
-                    <Image src="/icon-twitter.svg" alt='Go to twitter' width={50} height={50} />
+                <div className='flex gap-8'>
+                    <Image src="/icon-facebook.svg" alt='Go to facebook' width={25} height={25} />
+                    <Image src="/icon-twitter.svg" alt='Go to twitter' width={25} height={25} />
                 </div>
-                
             </div>
         </section>
     )
