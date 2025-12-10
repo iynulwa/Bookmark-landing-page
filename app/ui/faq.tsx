8ee2b162f9a62e9e87962cfface8d15f8f6be1c3
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-12-10 05:24:33 +0100

    styles: Styled FAQ section and added FAQ questions

--- app/ui/faq.tsx
+++ app/ui/faq.tsx
@@ -1,13 +1,35 @@
+'use client';
+
+import { useState } from "react";
+import faqData from '@/data/faq.json';
+import Image from "next/image";
+
 export default function Faq() {
+
+    const [activeQuestion, setActiveQuestion] = useState <number | null>(null);
+
     return (
-        <section>
-            <h2>Frequently asked questions</h2>
-            <p>
+        <section className="grid place-items-center gap-4">
+            <h2 className='text-2xl font-bold text-center'>Frequently asked questions</h2>
+            <p className='text-center text-width'>
                 Here are some of our FAQs. If you have any
                 other questions you'd like answered please feel
                 free to email us.
             </p>
-            <a>More Info</a>
+            <div>
+                {faqData.map((data) => (
+                    <div className="flex flex-col items-start" key={data.id}>
+                        <button
+                            onClick={() => setActiveQuestion(activeQuestion === data.id ? null : data.id)}
+                            className="flex w-full items-center justify-between py-6 gap-10">
+                            {data.question}
+                            {activeQuestion === data.id ? <Image className="up-arrow" src="/icon-arrow.svg" width={15} height={15} alt="" /> : <Image src="/icon-arrow.svg" width={15} height={15} alt="" />}
+                        </button>
+                        <hr className='h-px w-full bg-grey-100' />
+                    </div> 
+                ))}
+            </div>
+            <button className="my-10 py-4 px-6 rounded-lg bg-blue-600 text-white drop-shadow-md">More Info</button>
         </section>
     )
 }
