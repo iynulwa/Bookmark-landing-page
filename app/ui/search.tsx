8e29434dd39aa300e32684a31d28562a7f8c6804
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-12-15 17:20:02 +0100

    feat: Input validation added to contact us section in the footer

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,9 +1,41 @@
+'use client'
+
+import { useState } from "react";
+import Image from "next/image";
+
 export default function Search() {
+
+    const [input, setInput] = useState('');
+    const [errorState, setErrorState] = useState(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(event.target.value);
+    }
+
+    const validate = (email: string) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
+
+    const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        if (input.trim() === '' || !validate(input)) {
+            setErrorState(true)
+        } else {
+            setErrorState(false)
+        }
+    }
+
     return(
         <div className="md:flex md:gap-6 md:w-1/2">
-            <label htmlFor="email" className="sr-only"></label>
-            <input className="bg-white w-full p-4 rounded-md" id="email" type="email" name="email" placeholder="Enter your email address" required/>
-            <button className="w-full bg-[var(--clr-red-400)] text-white p-4 my-2 rounded-md md:my-0 md:w-1/2">Contact Us</button>
+            <label htmlFor="email" className="sr-only">Email</label>
+            <div>
+                <input onChange={handleChange} className="bg-white w-full p-4 rounded-md text-black" id="email" type="email" value={input} name="email" placeholder="Enter your email address" required/>
+                {errorState && <Image className="w-auto h-4" src='/icon-error.svg' alt="" width={25} height={12} />}
+                {errorState && <p className="bg-[var(--clr-red-400)] rounded-md p-2 text-white">Whoops, make sure it is an email</p>}
+            </div>
+            <button onClick={handleSubmit} className="w-full bg-[var(--clr-red-400)] text-white p-4 my-2 rounded-md md:my-0 md:w-1/2">Contact Us</button>
         </div>
     )
 }
